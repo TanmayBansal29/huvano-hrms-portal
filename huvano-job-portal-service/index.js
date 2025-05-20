74b6418a50d25d29ebcd44fee8e35e3e720ec008
Tanmay Bansal <bansaltanmay554@gmail.com>
2025-05-21 00:01:05 +0530

    Created Routes for Interview Invite

--- huvano-job-portal-service/index.js
+++ huvano-job-portal-service/index.js
@@ -5,6 +5,7 @@ const HRRoutes = require("./routes/HR")
 const JobPostRoutes = require("./routes/JobPost")
 const CandidateJobPostsRoutes = require("./routes/JobCandidate")
 const InterviewScheduleRoutes = require("./routes/InterviewScheduling")
+const InterviewInviteRoutes = require("./routes/InterviewInvite")
 require("dotenv").config()
 const connectDB = require("./config/database")
 const cookieParser = require("cookie-parser")
@@ -29,6 +30,7 @@ app.use("/api/v1/auth/HR", HRRoutes)
 app.use("/api/v1/job", JobPostRoutes)
 app.use("/api/v1/jobPosts", CandidateJobPostsRoutes)
 app.use("/api/v1/interviews", InterviewScheduleRoutes)
+app.use("/api/v1/interviews", InterviewInviteRoutes)
 
 // Connection to Database and Starting the server
 connectDB().then(() => {
---
+++ huvano-job-portal-service/routes/InterviewInvite.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const { auth, isCandidate } = require("../middlewares/auth")
+const { getInterviewDetails, updateResponse, requestReschedule } = require("../controllers/InterviewInvite")
+const router = express.Router()
+
+
+// Router for Candidate to get the details of interview Scheduled
+router.get("/get/interview/details/:jobId", auth, isCandidate, getInterviewDetails)
+
+// Router for Candidate to respond to the interview invite
+router.patch("/update/response/interview/:interviewId", auth, isCandidate, updateResponse)
+
+// Router for candidate to request for reschedule of interview
+router.patch("/request/reschedule/interview/:interviewId", auth, isCandidate, requestReschedule)
+
+module.exports = router
