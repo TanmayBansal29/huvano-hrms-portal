3136e7a486c004b41eb27501d3a24e017ce70a2d
Tanmay Bansal <bansaltanmay554@gmail.com>
2025-05-21 02:00:26 +0530

    Created Offer Letter Model

---
+++ huvano-job-portal-service/models/OfferLetter.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose")
+
+const OfferLetterSchema = mongoose.Schema({
+    candidateId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "CandidateProfile",
+        required: true
+    },
+    jobId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "JobPost",
+        required: true
+    },
+    applicationId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Application",
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["Pending", "Accepted", "Declined", "Revoked"],
+        default: "Pending"
+    },
+    content: {
+        type: String,
+        required: true,
+        minLength: 200,
+        maxLength: 400
+    },
+    issuedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "HRProfile"
+    },
+    issuedAt: {
+        type: Date,
+        default: Date.now
+    }
+}, {timestamps: true})
+
+const OfferLetter = mongoose.model("OfferLetter", OfferLetterSchema)
+module.exports = OfferLetter
