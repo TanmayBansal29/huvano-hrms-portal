31e8beeec846a9ebef2c48b60113f73c23e10c52
Tanmay Bansal <bansaltanmay554@gmail.com>
2025-06-04 01:27:29 +0530

    Completed model for user

---
+++ huvano-user-management-service/models/User.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose")
+const validator = require("validator")
+
+const userSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }, 
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    empId: {
+        type: true,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid Email")
+            }
+        }
+    },
+    password: {
+        type: String,
+        trim: true
+    },
+    role: {
+        type: String,
+        enum: ["Admin", "HR", "Employee", "Intern", "Team_Lead", "Manager", "Senior_Manager", "Associate_Director", "Director"],
+        required: true
+    },
+    level: {
+        type: Number,
+        enum: [1,2,3,4],
+        required: true
+    }
+})
+
+const Employee = mongoose.model("Employee", userSchema)
+module.exports = Employee
