8dd26615068f3404872dcd948f731da0f075616b
TanmayBansal29 <bansaltanmay554@gmail.com>
2025-06-08 16:27:17 +0530

    Created the routes for login and logout

--- huvano-user-management-service/routes/auth.js
+++ huvano-user-management-service/routes/auth.js
@@ -1,3 +1,9 @@
 const express = require("express")
+const { login, logout } = require("../controllers/Auth")
 const router = express.Router()
 
+// Created the routes for login and logout
+router.post("/login", login)
+router.post("/logout", logout)
+
+module.exports = router
